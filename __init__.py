5e0338897d02fb6390c418d4487125cea09f8f09
Arg!! <66618295+Argx2121@users.noreply.github.com>
2025-11-30 18:56:46 +1030

    Create __init__.py
    with bl_info

---
+++ __init__.py
@@ -0,0 +1,12 @@
+bl_info = {
+    "name": "Blender NSBMD",
+    "description": "Plugin to Export NSBMD models",
+    "author": "Yacker and Arg!!",
+    "version": (0, 0, 0),
+    "blender": (4, 1, 1),
+    "location": "3d View > Sidebar",
+    "warning": "",
+    "wiki_url": "",
+    "tracker_url": "https://github.com/Yackerw/Blender-NSBMD/issues/new",
+    "category": "Import-Export",
+}
