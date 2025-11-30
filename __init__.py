91f1909a4a2881dfced1213e26a90893e8b7415d
Argx2121 <sleepydersedereamer@gmail.com>
2025-11-30 19:44:15 +1030

    initial functions for init waow

--- __init__.py
+++ __init__.py
@@ -10,3 +10,50 @@ bl_info = {
     "tracker_url": "https://github.com/Yackerw/Blender-NSBMD/issues/new",
     "category": "Import-Export",
 }
+
+import sys
+
+# https://blenderartists.org/t/how-to-reload-blender-addon-with-nested-modules-using-reload-scripts/1477986/2
+if "bpy" in locals():
+    if __name__ in sys.modules:
+        del sys.modules[__name__]
+
+    dotted = __name__ + "."
+    for name in tuple(sys.modules):
+        if name.startswith(dotted):
+            del sys.modules[name]
+
+import bpy
+from bpy.app.handlers import persistent
+
+
+
+classes = [
+    #panels.MyPanel
+]
+
+
+# register
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    #bpy.types.NODE_MT_add.append(menus.my_node_menu)
+    #bpy.types.TOPBAR_MT_file_export.append(my_export.menu_func_export)
+    #my_props.custom_reg()
+
+
+def unregister():
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
+    #bpy.types.NODE_MT_add.remove(menus.my_node_menu)
+    #bpy.types.TOPBAR_MT_file_export.remove(my_export.menu_func_export)
+    #my_props.custom_unreg()
+
+# for when nodegroups are real
+'''@persistent
+def make_node_groups(scene):
+    MakeGroups().execute()
+
+
+bpy.app.handlers.load_post.append(make_node_groups)'''
+
